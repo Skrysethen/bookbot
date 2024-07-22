00b86d5ee4b163652f6891e754073fa16c397d83
Skrysethen <skryseth@gmail.com>
2024-07-22 16:15:13 +0200

    Update: main.py -> func read_book + count_words

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("hello world")
+
+def read_book(pathToBook):
+    with open(pathToBook) as f: 
+        content = f.read()
+        return content
+
+def count_words(stringToCount):
+    words = stringToCount.split()
+    c = 0
+    for i in words: 
+        c += 1
+    return c 
+
+def main():
+    pathToBook = "books/frankenstein.txt"
+    frankenstein = read_book(pathToBook)
+    count = count_words(frankenstein)
+    print(count)
+    
+
+main()
