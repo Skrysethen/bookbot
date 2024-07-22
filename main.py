bade54406d7520eec7f6b0cce1a3b7b7dc50a989
Skrysethen <skryseth@gmail.com>
2024-07-22 16:33:16 +0200

    Update: new function count_letters

--- main.py
+++ main.py
@@ -11,11 +11,24 @@ def count_words(stringToCount):
         c += 1
     return c 
 
+def count_letters(words):
+    letters_counted = {}
+    for l in words:
+        le = l.lower()
+        if le in letters_counted:
+            letters_counted[le] += 1
+        elif le.isalpha(): 
+            letters_counted[le] = 1
+    
+    return letters_counted
+        
+
 def main():
     pathToBook = "books/frankenstein.txt"
     frankenstein = read_book(pathToBook)
     count = count_words(frankenstein)
     print(count)
-    
+    letters = count_letters(frankenstein)
+    print(letters)
 
 main()
